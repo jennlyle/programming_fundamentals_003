61851cf1a1a5809aa1deaf7849e9727732cf2a3e
jennlyle <jennifer.g.lyle@gmail.com>
2020-03-26 13:35:06 +0000

    26-03-2020 Work

--- app/catalogue_service.js
+++ app/catalogue_service.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line no-unused-vars
+/*
 const catalogue = [
   "The Catcher in the Rye by J.D. Salinger (10)",
   "Dracula by Bram Stoker (0)",
@@ -21,29 +22,95 @@ const catalogue = [
   "Why Be Happy When You Could Be Normal? by Jeanette Winterson (19)",
   "The Origin of Species by Charles Darwin (50)"
 ];
+*/
+
+const catalogue = [
+  {title: "The Catcher in the Rye", author: "J.D. Salinger", quantity: 10},
+  {title: "Dracula", author: "Bram Stoker", quantity: 0},
+  {title: "Between the Assassinations", author: "Aravind Adiga", quantity: 9},
+  {title: "Wolf Hall", author: "Hilary Mantel", quantity: 33},
+  {title: "Bring Up The Bodies", author: "Hilary Mantel", quantity: 31},
+  {title: "A Place of Greater Safety", author: "Hilary Mantel", quantity: 11},
+  {title: "Giving Up the Ghost", author: "Hilary Mantel", quantity: 8},
+  {title: "The Assassination of Margaret Thatcher", author: "Hilary Mantel", quantity: 43},
+  {title: "The Yellow Wallpaper", author: "Charlotte Perkins Gilman", quantity: 12},
+  {title: "Conversations With Friends", author: "Sally Rooney", quantity: 1},
+  {title: "Normal People", author: "Sally Rooney", quantity: 2},
+  {title: "Everything I Never Told You", author: "Celeste Ng", quantity: 6},
+  {title: "2666", author: "Robert Bolaño", quantity: 17},
+  {title: "By Night In Chile", author: "Robert Bolaño", quantity: 8},
+  {title: "A Tale of Two Cities", author: "Charles Dickens", quantity: 3},
+  {title: "Oliver Twist", author: "Charles Dickens", quantity: 7},
+  {title: "Great Expectations", author: "Charles Dickens", quantity: 1},
+  {title: "The Blind Assassin", author: "Margaret Atwood", quantity: 8},
+  {title: "Why Be Happy When You Could Be Normal?", author: "Jeanette Winterson", quantity: 19},
+  {title: "The Origin of Species", author: "Charles Darwin", quantity: 50}
+];
 
 function countBooksByAuthor(author) {
   // Your code here
+  let count = 0;
+  for (i = 0; i < catalogue.length; i++) {
+    if (catalogue[i].author == author) {
+      count++;
+    }
+  }
+  return count;
 }
 
 function checkBookByTitle(title) {
   // Your code here
+  for (i = 0; i < catalogue.length; i++) {
+    if (catalogue[i].title == title) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function countBooksByFirstLetter(letter) {
   // Your code here
+  let count = 0;
+  for (i = 0; i < catalogue.length; i++) {
+    if (catalogue[i].title.charAt(0) == letter.toUpperCase()) {
+      count++; 
+    }
+  }
+  return count;
 }
 
 function getQuantity(title) {
   // Your code here
+  let quantity = 0;
+  for (i = 0; i < catalogue.length; i++) {
+    if (catalogue[i].title == title) {
+      quantity = catalogue[i].quantity;
+    }
+  }
+  return quantity;
 }
 
 function getBooksByAuthor(author) {
   // Your code here
+  let books = [];
+  let count = 0;
+  for (i = 0; i < catalogue.length; i++) {
+    if (catalogue[i].author == author) {
+      books[count] = catalogue[i];
+      count++;
+    }
+  }
+  return books;
 }
 
 function checkQuantity(title, quantity) {
   // Your code here
+  for (i = 0; i < catalogue.length; i++) {
+    if (catalogue[i].title == title && catalogue[i].quantity > quantity) {
+      return true;
+    }
+  }
+  return false; 
 }
 
 module.exports = {
--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -8,4 +8,42 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
     });
   });
+
+  describe("catalogueService.checkBookByTitle", () => {
+    test("returns true or false if the book exists in the catalogue", () => {
+      expect(catalogueService.checkBookByTitle("The Origin of Species")).toBe(true);
+      expect(catalogueService.checkBookByTitle("The Chronicles of Narnia")).toBe(false);
+    })
+  })
+
+  describe("catalogueService.countBooksByFirstLetter", () => {
+    test("returns number of titles that start with the given letter", () => {
+      expect(catalogueService.countBooksByFirstLetter("W")).toBe(2);
+      expect(catalogueService.countBooksByFirstLetter("w")).toBe(2);
+    })
+  })
+
+  describe("catalogueService.getQuantity", () => {
+    test("returns the quantity of books given the book title", () => {
+      expect(catalogueService.getQuantity("A Place of Greater Safety")).toBe(11);
+    })
+  })
+
+  describe("catalogueService.getBooksByAuthor", () => {
+    test("returns an array of books when given the author's name", () => {
+      expect(catalogueService.getBooksByAuthor("Robert Bolaño")).toEqual([
+        { title: "2666", author: "Robert Bolaño", quantity: 17 },
+        { title: "By Night In Chile", author: "Robert Bolaño", quantity: 8 }
+      ]);
+    })
+  })
+
+  describe("catalogueService.checkQuantity", () => {
+    test("returns true or false given a title and qty, if that title exists and stock is greater than quantity", () => {
+      expect(catalogueService.checkQuantity("By Night In Chile", 4)).toBe(true);
+      expect(catalogueService.checkQuantity("By Night In Chile", 100)).toBe(false);
+    })
+  })
+
+
 });
